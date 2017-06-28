3aeb349250854046587457a0ac49103eb085f206
Dan Bornman <danbornman@yahoo.com>
2017-06-28 14:00:50 -0400

    adding gas sensor sketch

--- Gas_sensor/Gas_sensor.ino
+++ Gas_sensor/Gas_sensor.ino
@@ -1,61 +1,46 @@
 #include <ArduinoJson.h>
 #include <MQTTClient.h>
 #include <ESP8266WiFi.h>
-#include "DHT.h"
-#include <TimeLib.h>
 
-
-#define DHTPIN 14
-#define DHTTYPE DHT11
-
-DHT dht(DHTPIN, DHTTYPE);
-
-int moisturePin = A0;
-int moistureValue = 0;
-
-int wifiLED = 14;
-int mqttLED = 13;
-int dataLED = 15;
-
-//const char* ssid = "VzMiFi6620L-BE7B";
-//const char* password = "4a23aab3";
-const char* ssid = "gateway-AP";
-const char* password = "iotgateway";
+const char* ssid = "iot-gateway";
+const char* password = "iot-nw-pilot13";
 
 const char* mqtt_server = "192.168.42.1";
-const char* deviceID = "dht11_bornman";
+const char* deviceID = "gas-mq4-001";
+const char* clientID = "gas-mq4-001";
 
+int minVal = 1000;
+int maxVal = 0;
 
+int infoLED = 14;
 
 WiFiClient wifiClient;
 MQTTClient mqttClient;
 DynamicJsonBuffer jsonBuffer;
 JsonObject& root = jsonBuffer.createObject();
-JsonArray& message = root.createNestedArray("message");
+JsonArray& message = root.createNestedArray("readings");
 JsonObject& data = message.createNestedObject();
 
 void connectWiFi(){
   delay(10);
   Serial.print("preparing wifi...");
+  
   WiFi.begin(ssid,password);
   while(WiFi.status() != WL_CONNECTED){
-    digitalWrite(wifiLED, LOW);
     Serial.print(".");
+    
     delay(200);
   }
-  digitalWrite(wifiLED, HIGH);
   Serial.println();
   Serial.println("Connected to WiFi!");
 }
 
 void reconnectMQTT(){
   while(!mqttClient.connected()){
-    digitalWrite(mqttLED, LOW);
     Serial.println("Connecting to Mqtt Broker...");
-    if(mqttClient.connect("ESP8266Client")){
-      digitalWrite(mqttLED, HIGH);
+    if(mqttClient.connect(clientID)){
       Serial.println("Connected to Broker!");
-      mqttClient.subscribe("/moistureSensor/command");
+      mqttClient.subscribe("/sensors/command");
     } else {
       Serial.println("Failed to connect to Mqtt Broker, retrying...");
       delay(2500);
@@ -63,57 +48,49 @@ void reconnectMQTT(){
   }
 }
 
+
 void setup() {
-  pinMode(wifiLED, OUTPUT);
-  pinMode(mqttLED, OUTPUT);
-  pinMode(dataLED, OUTPUT);
   Serial.begin(9600);
+  pinMode(infoLED, OUTPUT);
+
   connectWiFi();
   mqttClient.begin(mqtt_server, wifiClient);
 }
 
-
 void loop() {
   mqttClient.loop();
   delay(10);
-
+  
   if(!mqttClient.connected()){
     reconnectMQTT();
   } else {
-    moistureValue = analogRead(moisturePin);
-    float moistureF = (float) moistureValue;
-    float h = dht.readHumidity();
-    float t = dht.readTemperature(true);
-    if (isnan(h) || isnan(t)){
-      Serial.println("failed to read sensor");
-      return;
+    int gasLevel = analogRead(A0);
+    if (gasLevel < minVal){
+      minVal = gasLevel;
     }
-    
-    
+    if (gasLevel > maxVal){
+      maxVal = gasLevel;
+    }
+    Serial.print("level =  "); Serial.print(gasLevel);
+    Serial.print("\t"); Serial.print("min="); Serial.print(minVal);
+    Serial.print("\t"); Serial.print("max="); Serial.print(maxVal);
+
     root["deviceID"] = deviceID;
+    data["gas"] = gasLevel;
 
-    data["temp"] = double_with_n_digits(t,2);
-    data["humidity"] = double_with_n_digits(h,2);
-    data["moisture"] = double_with_n_digits(moistureF,1);
-
-    /*
-     * {"message":[{"temp":75.20,"humidity":31.00,"moisture":2.0}],"deviceID":"dht11_bornman"}
-     */
-     
+    Serial.print("\t");
     root.printTo(Serial);
     Serial.println();
-
     char buffer[256];
     root.printTo(buffer, sizeof(buffer));
-    //String jsonStr = String (buffer);
 
-    digitalWrite(dataLED, HIGH);
+    digitalWrite(infoLED, HIGH);
     mqttClient.publish("sensor", String (buffer));
     delay(100);
-    digitalWrite(dataLED, LOW);
-    delay(3000);
+    digitalWrite(infoLED, LOW);
     
-
+    delay(1000);
+    
   }
 }
 
@@ -125,7 +102,4 @@ void messageReceived(String topic, String payload, char* bytes, unsigned int len
   Serial.print("]");
   Serial.println();
 }
-
-
-
 
